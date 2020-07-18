3688fc13df590374d2bd36d4ca078549f88393e7
Gnlow <entrydark@gmail.com>
2020-07-18 21:01:18 +0900

    export

---
+++ mod.ts
@@ -0,0 +1,3 @@
+export {default as gender} from "https://raw.githubusercontent.com/randkid/gender/master/mod.ts"
+export {default as bloodType} from "https://raw.githubusercontent.com/randkid/bloodType/master/mod.ts"
+export {default as name} from "https://raw.githubusercontent.com/randkid/name/master/mod.ts"
